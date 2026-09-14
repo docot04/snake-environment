c04be3c00cad6bfc1b6abfaa8e2bb7cb1dfc5b37
docot04 <nihonfang04@gmail.com>
2026-09-14 23:48:00 +0530

    random agent

--- src/agent.c
+++ src/agent.c
@@ -1,12 +1,18 @@
-// #include "snake.h"
+#include "snake.h"
 
-// Game game;
-
-// reset(&game);
-
-// while(!is_done(&game)){
-//     get_state(&game, state);
-//     action = agent_choose_action(state);
-//     consequence = step(&game, action);
-//     agent_learn(state, action, consequence);
-// }
+int main() {
+    snake_seed(42);
+    Snake game;
+    snake_reset(&game);
+    while (!snake_is_done(&game)) {
+        int state[STATE_SIZE];
+        snake_get_state(&game, state);
+        Action action = (Action)(rand() % 3);
+        float reward = snake_step(&game, action);
+        printf("Action: %d | Reward: %.1f\n", action, reward);
+        print_state(&game);
+    }
+    printf("\nGame Over!\n");
+    printf("Final score: %d\n", game.score);
+    return 0;
+}
